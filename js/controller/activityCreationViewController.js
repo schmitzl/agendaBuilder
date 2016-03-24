c3e6fd2bb4f5338aca6dc2cb095b6fca8cead144
tomaraki <krinaki.m@gmail.com>
2016-03-24 23:07:01 +0100

    Check input and give feedback in the " create new activity" form. Done

--- js/controller/activityCreationViewController.js
+++ js/controller/activityCreationViewController.js
@@ -25,24 +25,46 @@ var ActivityCreationViewController = function (activityCreationView, agendaModel
     });
 	
 	// Adding new activity to model
-	$('#submitNewActivityButton').on('click', function(){
-       if(val = $('#activityLengthInMin').val(), $.isNumeric(val) && Math.floor(val) == val){
-            var activityLength = parseInt($('#activityLengthInMin').val());
-            if( activityLength > 1440){
-                $('#lengthError').html("Insert a smaller activity lenght!");
+    $('#submitNewActivityButton').on('click', function(){
+        var temp1=0;
+        var temp2=0;
+       
+            var str = $('#activityTitle').val();
+            var title = str.trim().length;
+            $('#titleError').html("");
+            if( title > 25 || title < 1){
+                    $('#titleError').html("Enter 1-25 characters!");
+            }
+            else{
+                temp1= 1;
+            }
+            var activityLenght = parseInt($('#activityLengthInMin').val());
+            if(val = $('#activityLengthInMin').val(), $.isNumeric(val) && Math.floor(val) == val){
+                $('#lengthError').html("");
+                if( activityLenght > 1440){
+                    $('#lengthError').html("Enter a shorter activity length!");
+                } 
+                else if(activityLenght <= 0){
+                    $('#lengthError').html("Enter a positive number!");
+                }
+                else{
+                    temp2 = 1;
+                }
+
             } 
-            else if(activityLength <= 0){
-                $('#lengthError').html("Please insert a positive number!");
+            else
+            {
+                $('#lengthError').html("Enter a number!");
             }
-            else {
-                agendaModel.addParkedActivity(new Activity( $('#activityTitle').val(), parseInt($('#activityLengthInMin').val()), $('#activityType').val(), $('#activityDescription').val(), activityIdCounter), null);
-                activityIdCounter++;
-                hideCreationContainer();
-            }
-        } 
-        else{
-            $('#lengthError').html("Your input has to be a number!");
+
+        if (temp1 === 1 && temp2 === 1){
+        agendaModel.addActivity(new Activity( $('#activityTitle').val() , parseInt($('#activityLengthInMin').val()) , $('#activityType').val() , $('#activityDescription').val() ),0);
+                    hideCreationContainer();
         }
+        
+        
+        
+        
     });
     
 	$('#cancelNewActivityButton').on('click', function(){
